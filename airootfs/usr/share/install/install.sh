a6ca2b36b526d5a12554911acb58fc5e9caa82f4
luigiano-code <luigiano16@proton.me>
2026-01-30 19:50:55 +0100

    Cleaned Code

--- airootfs/usr/share/install/install.sh
+++ airootfs/usr/share/install/install.sh
@@ -5,21 +5,16 @@ set -euo pipefail
 DISK="/dev/vda"
 MNT="/mnt"
 
-# wyczyść tablicę partycji
 parted --script "$DISK" mklabel gpt
 
-# 1. partycja – 2GB na boot
 parted --script "$DISK" mkpart primary fat32 1MiB 2049MiB
 parted --script "$DISK" set 1 esp on
 
-# 2. partycja – reszta na root
 parted --script "$DISK" mkpart primary ext4 2049MiB 100%
 
-# formatuj
 mkfs.fat -F32 "${DISK}1"
 mkfs.ext4 -F "${DISK}2"
 
-# montowanie
 mount "${DISK}2" "$MNT"
 mkdir -p "$MNT"/boot
 mount "${DISK}1" "$MNT"/boot
